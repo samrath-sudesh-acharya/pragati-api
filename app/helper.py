84c133339c233907cb99d8b59e78d50430dc0cd7
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-10-13 19:25:03 +0530

    DEBUG dump data error

--- app/helper.py
+++ app/helper.py
@@ -2,12 +2,12 @@ import json
 
 def read_json():
     "Return json data"
-    f = open("app/data/college.json")
+    f = open("app/college.json")
     data = json.load(f)
     return data
 
 def read_json_id(id:int):
     "Return json data with id"
-    f = open("app/data/college.json")
+    f = open("app/college.json")
     data = json.load(f)
     return data[id-1]
